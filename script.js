5ae073ef62203d1a9fa7e01ef844689944ce0f11
Kristy Huang <khuang@yext.com>
2022-01-24 21:57:21 -0500

    program structure

---
+++ script.js
@@ -0,0 +1,40 @@
+// object containing array of names, messages, and 
+const messages = {
+    names: [
+        'Susie Xu',
+        'Jeanne Hou',
+        'Fed Finjap',
+        'Amani Farooque',
+        'Sam Torres',
+        'Victor Dadfar',
+        'Chris Heagan',
+        "Wesley Jia",
+        "Alma Chang",
+        "Jukes Santra"
+    ],
+    inspiration: [
+        'Have more fear of regret than failure.',
+        'The only person you should try to be better than is the person you were yesterday.',
+        'Do somethign today that your future self will thank you for.',
+        'Don\'t quit. Suffer now and live the rest of your life as a champion.',
+        'You don\'t get results by focusing on results. You get results by focusing on the actions that produce results.',
+        'Don\'t try to build the greatest wall that\'s ever been built. Focus on laying a single, expertly-placed brick. Then keep doing that, every day.'
+    ],
+    practical: [
+        'Drink water',
+        'Go outside for a walk',
+        'Stand up and stretch',
+        'Remember to do your laundry',
+        'Do your daily Wordle'
+    ]
+}
+
+
+// function to pick a random item from an array
+const randomItem = array => {
+
+}
+
+const displayMessage = 'test';
+
+console.log(displayMessage);
